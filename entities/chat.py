3d7365226e75232b93429db7c617e9d35cbad348
XiuyuanLu <276908010@qq.com>
2023-07-23 18:08:12 +0800

    for test

--- entities/chat.py
+++ entities/chat.py
@@ -3,6 +3,7 @@ import openai
 
 def talk_to_gpt(api_key, messages, model='gpt-3.5-turbo') -> str:
     openai.api_key = api_key
-    response = openai.ChatCompletion.create(model=model, messages=messages)
+    serialized_msgs = [x.serialise for x in messages]
+    response = openai.ChatCompletion.create(model=model, messages=serialized_msgs)
     return response.choices[0].message.content
     # return 'B.'
